0a36579fc823182fa14121335c3d57ef08a3cf0c
Simon Vestergaard Laursen <svl@gavdilabs.com>
2025-05-30 15:33:53 +0200

    chore(gitignore): Removed previously committed files that should be ignored

--- @cds-models/com/gavdilabs/techtransmgt/types/index.js
+++
@@ -1,7 +0,0 @@
-// This is an automatically generated file. Please do not change its contents manually!
-const { createEntityProxy } = require('./../../../../_')
-// ActiveUser
-module.exports.ActiveUser = createEntityProxy(['com.gavdilabs.techtransmgt.types', 'ActiveUser'], { target: { is_singular: true } })
-// events
-// actions
-// enums
