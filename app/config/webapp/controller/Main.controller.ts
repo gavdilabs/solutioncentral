aa66b65a023d0465943d8c9ecf95d13b8796b54b
Morten Brink Kristensen <43001847+mokgavdilabs@users.noreply.github.com>
2025-06-10 11:06:21 +0200

    feat(config): Configuration App - Validations added

--- app/config/webapp/controller/Main.controller.ts
+++ app/config/webapp/controller/Main.controller.ts
@@ -1,4 +1,5 @@
 import MessageBox from "sap/m/MessageBox";
+//import Action from "sap/m/MessageBox";
 import BaseController from "./BaseController";
 import List from "sap/m/List";
 import ODataListBinding from "sap/ui/model/odata/v4/ODataListBinding";
@@ -9,6 +10,12 @@ import Dialog from "sap/m/Dialog";
 import Fragment from "sap/ui/core/Fragment";
 import Context from "sap/ui/model/odata/v4/Context";
 import { Icon$PressEvent } from "sap/ui/core/Icon";
+import Input from "sap/m/Input";
+import Select from "sap/m/Select";
+import CheckBox from "sap/m/CheckBox";
+import ComboBox from "sap/m/ComboBox";
+import DatePicker from "sap/m/DatePicker";
+import Control from "sap/ui/core/Control";
 
 /**
  * @namespace com.gavdilabs.config.controller
@@ -16,6 +23,8 @@ import { Icon$PressEvent } from "sap/ui/core/Icon";
 export default class Main extends BaseController {
 
 	private _userDialog: Dialog;
+	private _teamDialog: Dialog;
+
 	public onInit(): void {
 		
 	}
@@ -30,19 +39,34 @@ export default class Main extends BaseController {
 	}
 
 	public async onButtonAddUserPress() {
-		const oModel = this.getView().getModel() as ODataModel;
-		//const oTable = this.getView().byId("idSoftwareTeamUserTable") as Table;
-		const oContext = oModel.bindList("/User", undefined, undefined, undefined, { $$updateGroupId: 'userUpdateGroup' }).create();
+		//const oModel = this.getView().getModel() as ODataModel;
+		//const oContext = oModel.bindList("/User", undefined, undefined, undefined, { $$updateGroupId: 'userUpdateGroup' }).create();
+		const oTable = this.getView().byId("idUserTable") as Table;
+		const oContext = (oTable.getBinding("items") as ODataListBinding).create();		
 		await this.openUserDialog(oContext);
 	}
 
 	public async onIconEditUserPress(oEvent: Icon$PressEvent) {
 		const oContext = oEvent.getSource().getBindingContext() as Context;
-		const oModel = this.getView().getModel() as ODataModel;
-		const sUserName = oContext.getProperty("user_username") as string;		
-		const oBinding = oModel.bindContext(`/User('${sUserName}')`);
-		const oNewContext = oBinding.getBoundContext();		
-		await this.openUserDialog(oNewContext);	
+		await this.openUserDialog(oContext);	
+	}
+
+	public onIconDeleteUserPress(oEvent: Icon$PressEvent) {
+		MessageBox.confirm(this.getText('msgDeleteUser'), {
+			actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
+			emphasizedAction: MessageBox.Action.OK,
+			onClose: async (sAction: string ) => {
+				if(sAction === 'OK') {
+					const oContext = oEvent.getSource().getBindingContext() as Context;					
+					await oContext.delete('$auto').then( () => {
+						MessageBox.success(this.getText("msgUserSaved"));
+					})
+					.catch(() => {						
+						MessageBox.error(this.getText("msgUserSaveError"));
+					});		
+				} 
+			}
+		});		
 	}
 
 	private async openUserDialog(oContext: Context) {
@@ -56,29 +80,25 @@ export default class Main extends BaseController {
 	}
 	
 	public async onSaveButtonUserPress() {
-		const sUserName = (this._userDialog.getBindingContext() as Context).getProperty("username") as string;
-		const oTable = Fragment.byId("UserDialog","idSoftwareTeamsTable") as Table;
-		const aItems = oTable.getItems();
-
-		for (const oItem of aItems) {		
-			const oContext = oItem.getBindingContext() as Context;
-			await oContext.setProperty("user_username",sUserName);
-		};
-
-		const oModel = this.getView().getModel() as ODataModel;
-		this._userDialog.setBusy(true);
-		try {
-			await oModel.submitBatch("userUpdateGroup");
-			MessageBox.success(this.getText("msgUserSaved"));
-			this._userDialog.close();
-		} catch (catchError) {
-			console.error(catchError);
-			MessageBox.error(this.getText("msgUserSaveError"));
+		const aControls = this.getView().getControlsByFieldGroupId("UserFormGroup");
+		if(this.validateInput(aControls)) {
+			const oModel = this.getView().getModel() as ODataModel;
+			this._userDialog.setBusy(true);
+			try {
+				await oModel.submitBatch("userUpdateGroup");
+				MessageBox.success(this.getText("msgUserSaved"));				
+				this._userDialog.close();
+			} catch (catchError) {
+				console.error(catchError);
+				MessageBox.error(this.getText("msgUserSaveError"));
+			}
+			this._userDialog.setBusy(false);			
 		}
-		this._userDialog.setBusy(false);
 	}
 	
 	public onCancelButtonUserPress() {
+		const oModel = this.getView().getModel() as ODataModel;
+		oModel.resetChanges("userUpdateGroup");
 		this._userDialog.close();	
 	}
 	
@@ -87,10 +107,67 @@ export default class Main extends BaseController {
 		(oTable.getBinding("items") as ODataListBinding).create();
 	}
 
-	public onButtonAddTeamPress() {
+	public async onButtonAddTeamPress() {
 		const oTable = this.getView().byId("idSoftwareTeamTable") as Table;
-		(oTable.getBinding("items") as ODataListBinding).create();
+		const oContext = (oTable.getBinding("items") as ODataListBinding).create();
+		await this.openTeamDialog(oContext);
 	}
+
+	public async onIconEditTeamPress(oEvent: Icon$PressEvent) {
+		const oContext = oEvent.getSource().getBindingContext() as Context;
+		await this.openTeamDialog(oContext);	
+	}
+
+	public onIconDeleteTeamPress(oEvent: Icon$PressEvent) {		
+		MessageBox.confirm(this.getText('msgDeleteTeam'), {
+			actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
+			emphasizedAction: MessageBox.Action.OK,
+			onClose: async (sAction: string ) => {
+				if(sAction === 'OK') {
+					const oContext = oEvent.getSource().getBindingContext() as Context;
+					await oContext.delete('$auto').then( () => {
+						MessageBox.success(this.getText("msgUserSaved"));
+					})
+					.catch(() => {						
+						MessageBox.error(this.getText("msgUserSaveError"));
+					});				
+				} 
+			}
+		});		
+	}
+
+	private async openTeamDialog(oContext: Context) {
+		if (!this._teamDialog) {
+			this._teamDialog ??= (await Fragment.load({id: "TeamDialog", name: "com.gavdilabs.config.view.fragments.TeamDialog", controller: this })) as Dialog;
+			this.getView().addDependent(this._teamDialog);
+		}		
+
+		this._teamDialog.setBindingContext(oContext);
+		this._teamDialog.open();	
+	}
+
+	public onCancelButtonTeamPress() {
+		const oModel = this.getView().getModel() as ODataModel;
+		oModel.resetChanges("userUpdateGroup");
+		this._teamDialog.close();	
+	}	
+
+	public async onSaveButtonTeamPress() {
+		const aControls = this.getView().getControlsByFieldGroupId("TeamFormGroup");
+		if(this.validateInput(aControls)) {
+			const oModel = this.getView().getModel() as ODataModel;
+			this._teamDialog.setBusy(true);
+			try {
+				await oModel.submitBatch("userUpdateGroup");
+				MessageBox.success(this.getText("msgUserSaved"));
+				this._teamDialog.close();
+			} catch (catchError) {
+				console.error(catchError);
+				MessageBox.error(this.getText("msgUserSaveError"));
+			}
+			this._teamDialog.setBusy(false);
+		}
+	}	
 
 	public onButtonAddConfigPress() {
 		const oList = this.getView().byId("idCompanyConfigurationList") as List;
@@ -98,29 +175,48 @@ export default class Main extends BaseController {
 	}
 	
 	public async onSaveConfigButtonPress() {
-		const oModel = this.getView().getModel() as ODataModel;
-		this.getView().setBusy(true);
-		try {
-			await oModel.submitBatch("configUpdateGroup");
-			MessageBox.success(this.getText("msgConfigSaved"));
-		} catch (catchError) {
-			console.error(catchError);
-			MessageBox.error(this.getText("msgConfigSaveError"));
+		const aControls = this.getView().getControlsByFieldGroupId("CompanyFormGroup");
+		if(this.validateInput(aControls)) {
+			const oModel = this.getView().getModel() as ODataModel;
+			this.getView().setBusy(true);
+			try {
+				await oModel.submitBatch("configUpdateGroup");
+				MessageBox.success(this.getText("msgConfigSaved"));
+			} catch (catchError) {
+				console.error(catchError);
+				MessageBox.error(this.getText("msgConfigSaveError"));
+			}
+			this.getView().setBusy(false);
 		}
-		this.getView().setBusy(false);
 	}
 
-	public async onSaveTeamsButtonPress() {
-		const oModel = this.getView().getModel() as ODataModel;
-		this.getView().setBusy(true);
-		try {
-			await oModel.submitBatch("userUpdateGroup");
-			MessageBox.success(this.getText("msgUserSaved"));
-		} catch (catchError) {
-			console.error(catchError);
-			MessageBox.error(this.getText("msgUserSaveError"));
-		}
-		this.getView().setBusy(false);
+	private validateInput(aControls: Control[]): boolean {		
+		let bValid: boolean = true;
+
+		aControls.forEach((oControl) => {		
+			if (oControl instanceof Input || oControl instanceof DatePicker) {
+				if ( oControl.getRequired() && oControl.getVisible() && !oControl.getValue() ) {
+					oControl.setValueState("Error");
+					bValid = false;
+				} else if (oControl.getRequired()) {
+					oControl.setValueState("None");
+				}
+			} else if (oControl instanceof Select || oControl instanceof ComboBox) {
+				if ( oControl.getRequired() && oControl.getVisible() && !oControl.getSelectedItem() ) {
+				oControl.setValueState("Error");
+					bValid = false;
+				} else {
+					oControl.setValueState("None");
+				}
+			} else if (oControl instanceof CheckBox) {
+				if (oControl.hasStyleClass("gavdiRequired") && oControl.getVisible() && !oControl.getSelected() ) {
+					oControl.setValueState("Error");
+					bValid = false;
+				} else {
+					oControl.setValueState("None");
+				}
+			}
+		});		
+		return bValid;
 	}
-	
 }
