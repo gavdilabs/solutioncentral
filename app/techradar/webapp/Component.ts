e7ab8421d09c3685ec4730ae2d09e167bbb17eb3
EmilVM <evm@gavdilabs.com>
2026-03-06 09:02:11 +0100

    feat(ui): added timeout handling for techradar and config app

--- app/techradar/webapp/Component.ts
+++ app/techradar/webapp/Component.ts
@@ -1,7 +1,17 @@
 import UIComponent from "sap/ui/core/UIComponent";
 import models from "./model/models";
 import Device from "sap/ui/Device";
+import IllustrationPool from "sap/m/IllustrationPool";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import Dialog from "sap/m/Dialog";
+import IllustratedMessage from "sap/m/IllustratedMessage";
+import Button from "sap/m/Button";
+import { ButtonType } from "sap/m/library";
 
+const TIMEOUT_WARNING_TIME: number = 5000; //Milliseconds : 13 minutes
+const TIMEOUT_TIME: number = 10000; // 15 minutes
+// const TIMEOUT_WARNING_TIME: number = 780000; //Milliseconds : 13 minutes
+// const TIMEOUT_TIME: number = 900000; // 15 minutes
 /**
  * @namespace com.gavdilabs.techradar
  */
@@ -11,6 +21,9 @@ export default class Component extends UIComponent {
 	};
 
 	private contentDensityClass: string;
+	private warningTimer: number;
+	private timeoutTimer: number;
+	private timeoutDialog: Dialog;
 
 	public init(): void {
 		// call the base component's init function
@@ -21,6 +34,18 @@ export default class Component extends UIComponent {
 
 		// create the views based on the url/hash
 		this.getRouter().initialize();
+
+		const tntSetConfig = {
+			setFamily: "tnt",
+			setURI: sap.ui.require.toUrl("sap/tnt/themes/base/illustrations"),
+		};
+
+		// register tnt illustration set
+		IllustrationPool.registerIllustrationSet(tntSetConfig, false);
+
+		void this.resetTimers();
+		document.onkeydown = this.resetTimers.bind(this);
+		document.onmousemove = this.resetTimers.bind(this);
 	}
 
 	/**
@@ -46,5 +71,62 @@ export default class Component extends UIComponent {
 			}
 		}
 		return this.contentDensityClass;
+	}
+
+	private async resetTimers() {
+		clearTimeout(this.warningTimer);
+		clearTimeout(this.timeoutTimer);
+
+		const model = this.getModel("i18n") as ResourceModel;
+		const i18n = await model.getResourceBundle();
+
+		this.warningTimer = setTimeout(() => {
+			this.timeoutDialog = new Dialog({
+				title: i18n.getText("timeout.title"),
+				contentWidth: "20rem",
+				content: new IllustratedMessage({
+					title: i18n.getText("timeout.warningTitle"),
+					illustrationType: "tnt-SessionExpiring",
+					description: i18n.getText("timeout.warningDesc"),
+				}),
+				beginButton: new Button({
+					type: ButtonType.Emphasized,
+					text: i18n.getText("timeout.warningConfirm"),
+					press: () => this.timeoutDialog.close(),
+				}),
+				afterClose: () => this.timeoutDialog.destroy(),
+			});
+			this.timeoutDialog.open();
+		}, TIMEOUT_WARNING_TIME);
+
+		this.timeoutTimer = setTimeout(() => {
+			if (this.timeoutDialog) {
+				if (this.timeoutDialog.isOpen()) this.timeoutDialog.close();
+				this.timeoutDialog.destroy();
+			}
+
+			const dialog = new Dialog({
+				title: i18n.getText("timeout.title"),
+				contentWidth: "20rem",
+				content: new IllustratedMessage({
+					title: i18n.getText("timeout.timedOutTitle"),
+					illustrationType: "tnt-SessionExpired",
+					description: i18n.getText("timeout.timedOutDesc"),
+				}),
+				beginButton: new Button({
+					type: ButtonType.Emphasized,
+					text: i18n.getText("timeout.timedOutConfirm"),
+					press: () => {
+						dialog.close();
+						window.location.reload();
+					},
+				}),
+				escapeHandler: () => {
+					dialog.close();
+					window.location.reload();
+				},
+			});
+			dialog.open();
+		}, TIMEOUT_TIME);
 	}
 }
